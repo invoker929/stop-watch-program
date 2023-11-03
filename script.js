4cefe16437cac1209464cadbd286e7faeba07c33
Invoker <mgsaw929@gmail.com>
2023-11-03 20:33:34 +0630

    Start Project

---
+++ script.js
@@ -0,0 +1,5 @@
+const timeContainer = document.querySelector(".time-container");
+const timeDisplay = document.querySelector(".time-display");
+
+console.log(timeContainer);
+console.log(timeDisplay);
